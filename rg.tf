b8e2e04c5db605fc43282550b863999cc5788e44
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2026-02-05 11:32:51 +0530

    Add Azure resource group configuration

---
+++ rg.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {
+  }
+subscription_id = "2df30ff1-915d-4d35-974a-3d3155aaa413"
+}
+resource "azurerm_resource_group" "new-rg1" {
+  name     = "shruti-rg2"
+  location = "westus"
+}
