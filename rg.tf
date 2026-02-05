47f3d766b545f99038dda746d0bf644c4df35154
shruti-linux <131751004+shruti-linux@users.noreply.github.com>
2026-02-05 18:32:53 +0530

    Rename resource group from new-rg1 to new-rg56

--- rg.tf
+++ rg.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   }
 subscription_id = "2df30ff1-915d-4d35-974a-3d3155aaa413"
 }
-resource "azurerm_resource_group" "new-rg1" {
-  name     = "shruti-rg2"
+resource "azurerm_resource_group" "new-rg56" {
+  name     = "shruti-rg56"
   location = "westus"
 }
